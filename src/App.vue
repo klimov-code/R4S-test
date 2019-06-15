d37d34f0d14a7042e01ee74ab569284e2936cc30
Ilya Klimov <therapyspb@yandex.ru>
2019-06-15 17:52:56 +0300

    added Table and Dropdown components

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Dropdown></Dropdown>
+    <Table></Table>
+  </div>
+</template>
+
+<script>
+import Dropdown from "./components/Dropdown";
+import Table from "./components/Table";
+
+export default {
+  name: "App",
+  components: {
+    Dropdown,
+    Table
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-dropdown :text="selected.length + ' Columns selected'">
+    <b-dropdown-form>
+      <b-form-checkbox v-model="all" @change="toggle">{{ 'Select All' }}</b-form-checkbox>
+      <b-form-checkbox-group v-model="selected" :options="options" stacked></b-form-checkbox-group>
+    </b-dropdown-form>
+  </b-dropdown>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  data() {
+    return {
+      all: true,
+      selected: ["product", "calories", "fat", "carbs", "protein", "iron"],
+      options: [
+        { text: "Dessert (100g serving)", value: "product" },
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" }
+      ]
+    };
+  },
+  watch: {
+    selected(newVal, oldVal) {
+      if (newVal.length === 0) {
+        this.all = false;
+      } else if (newVal.length === this.options.length) {
+        this.all = true;
+      } else {
+        this.all = false;
+      }
+    }
+  },
+  methods: {
+    toggle(checked) {
+      this.selected = checked
+        ? this.options.slice().map(({ value }) => value)
+        : [];
+    }
+  }
+};
+</script>
