d0c5c40261c0a46998e34d0844912e27ccdeef5d
Ilya Klimov <therapyspb@yandex.ru>
2019-06-15 23:23:28 +0300

    add GroupBy buttons

---
+++ src/components/TableButtonGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container>
+    <b-button
+      v-for="(button, index) of options"
+      :key="index"
+      @click="selectedGroupBy(button.value)"
+    >{{ button.text }}</b-button>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "TableButtonGroup",
+  data() {
+    return {
+      groupBy: "",
+      options: [
+        { text: "Dessert (100g serving)", value: "product" },
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" }
+      ]
+    };
+  },
+  methods: {
+    selectedGroupBy(value) {
+      this.groupBy = value;
+    }
+  }
+};
+</script>
