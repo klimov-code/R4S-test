331878b7ee4ec333826d7b4996fc4f90f9f4239e
Ilya Klimov <therapyspb@yandex.ru>
2019-06-16 17:51:23 +0300

    added mutations for table controls

--- src/store/modules/table/mutations.js
+++ src/store/modules/table/mutations.js
@@ -1,11 +1,17 @@
-import { types } from './types';
+import { types } from './mutation-types';
 
 const mutations = {
   [types.FETCH_PRODUCTS](state, payload) {
     state.products = payload.products;
   },
-  [types.DELETE_PRODUCT](state, payload) {
-    const deleteProducts = payload.deleteProducts;
+  [types.PREV_PAGE](state) {
+    state.currentPage -= 1;
+  },
+  [types.NEXT_PAGE](state) {
+    state.currentPage += 1;
+  },
+  [types.PER_PAGE](state, payload) {
+    state.perPage = payload.perPage;
   }
 };
 
