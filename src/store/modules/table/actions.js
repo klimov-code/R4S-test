cfd053df7c2141725f4ba17d6cfed9e6bdc58c3c
Ilya Klimov <therapyspb@yandex.ru>
2019-06-16 17:50:59 +0300

    added actions for table controls

--- src/store/modules/table/actions.js
+++ src/store/modules/table/actions.js
@@ -1,3 +1,26 @@
-const actions = {};
+import { emulateGetRequest } from '../../../api/request';
+
+import { types } from './mutation-types';
+
+const actions = {
+  async fetchProducts({ commit }) {
+    try {
+      const products = await emulateGetRequest();
+
+      commit({ type: types.FETCH_PRODUCTS, products });
+    } catch (error) {
+      console.warn(error);
+    }
+  },
+  prevPage({ commit }) {
+    commit({ type: types.PREV_PAGE });
+  },
+  nextPage({ commit }) {
+    commit({ type: types.NEXT_PAGE });
+  },
+  perPage({ commit }, payload) {
+    commit({ type: types.PER_PAGE, ...payload });
+  }
+};
 
 export { actions };
